8d694ecad534f2d8c0fdaffbfd58e18108b5b03a
ramababu.bendalam <ramababu.bendalam@adstream.com>
2016-11-16 16:34:50 +0000

    New java class

---
+++ src/com/company/Test.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by Ramababu.Bendalam on 16/11/2016.
+ */
+public class Test {
+
+    int duration;
+    String advertiser;
+    Boolean watermarkingInitialised;
+    String unique;
+    String closedCaptionStatus;
+    String _id;
+    String title;
+    String tapeNumber;
+    String masterArrivedComment;
+
+
+}
