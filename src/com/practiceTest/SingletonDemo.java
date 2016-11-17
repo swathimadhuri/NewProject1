b5f45ffcc0615d942ebc10ec89c9c9c34bf11915
Swathi.Battula <swathi.battula@adstream.com>
2016-11-17 15:55:29 +0000

    Added Singleton scripts

---
+++ src/com/practiceTest/SingletonDemo.java
@@ -0,0 +1,13 @@
+package com.practiceTest;
+
+/**
+ * Created by Swathi.Battula on 17/11/2016.
+ */
+public class SingletonDemo {
+    public static void main(String []args){
+        Singleton tmp = Singleton.getInstance();
+        tmp.demomethod();
+    }
+
+
+}
